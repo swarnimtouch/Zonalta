d558f566794201b607f6f0c1f871666f1361d234
prem <prem.swarnimtouch@gmail.com>
2026-03-25 07:14:41 +0000

    when admin logout so redirect employee login so issue solve

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -52,7 +52,7 @@ class AdminController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('login');
+        return redirect()->route('admin.login');
     }
 
     public function dashboard()
